5cc5b5f4099d3a783203ba77c373891fb92163a0
Fanny Gautier <fannygautier92@gmail.com>
2022-10-22 17:03:04 +0200

    :sparkles:add menu burger

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 'use strict'
 
-let histoireButton=document.querySelector(".histoire");
-let nowButton=document.querySelector(".now");
-let gallerieButton=document.querySelector(".galle");
+let menuBurgerIcone=document.querySelector(".burger-menu");
+let menuBurger=document.querySelector(".menu")
+let nav=document.querySelector("nav");
+
+
+menuBurgerIcone.addEventListener("click",() => {
+    nav.classList.toggle("transition-menu")
+    menuBurger.classList.toggle("menu-responsive")
+    document.querySelector(".une").classList.toggle("barre-une")
+    document.querySelector(".deux").classList.toggle("barre-deux")
+    document.querySelector(".trois").classList.toggle("barre-trois")
+   
+})
+
 
